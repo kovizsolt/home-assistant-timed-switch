2b47f9091ed013c69d5c820e1e113620b152f730
Home-Zsolt <zsolt@Home-Zsolt>
2026-09-02 20:30:41 +0200

    Release Timed Switch v0.3.0048

--- custom_components/timed_switch/_version.py
+++ custom_components/timed_switch/_version.py
@@ -1,5 +1,5 @@
 """Generated version metadata. Do not edit manually."""
 
-__version__ = "0.2.0048"
-__version_tuple__ = (0, 2, 48)
-__version_time__ = "2026-08-29T20:45:57+02:00"
+__version__ = "0.3.0048"
+__version_tuple__ = (0, 3, 48)
+__version_time__ = "2026-09-02T20:30:24+02:00"
